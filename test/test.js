08daa7ef15637d0e418b0263f03a949d1767e3d5
Eric Baer <me@ericbaer.com>
2015-12-11 12:03:07 -0800

    Refactor to iterate over all test directories

--- test/test.js
+++ test/test.js
@@ -11,21 +11,33 @@ var Promise = require("bluebird");
 var fs = Promise.promisifyAll(require("fs"));
 var transformFile = Promise.promisify(babel.transformFile);
 
-var options = {
-  "plugins": [ "../lib/index.js" ]
-};
+// TODO: Refactor to use Promises
+function getDirectories(srcpath) {
+  return fs.readdirSync(srcpath).filter(function(file) {
+    return fs.statSync(path.join(srcpath, file)).isDirectory();
+  });
+}
 
-Promise.props({
-  actual: transformFile(path.join(__dirname, "/define-messages/actual.js"), options)
-    .then(result => result.code)
-    .then(_.trim),
-  expected: fs.readFileAsync(path.join(__dirname, "/define-messages/expected.js"), "utf8")
-    .then(_.trim),
-})
-.then((result) => {
-  var diff = jsdiff.diffTrimmedLines(result.actual, result.expected);
-  assert.equal(diff.length, 1);
-});
+var assertTransformation = function (directoryName) {
+  var options = {
+    "plugins": [ "../lib/index.js" ]
+  };
+
+  Promise.props({
+    actual: transformFile(path.join(__dirname, "/" + directoryName + "/actual.js"), options)
+      .then(result => result.code)
+      .then(_.trim),
+    expected: fs.readFileAsync(path.join(__dirname, "/" + directoryName + "/expected.js"), "utf8")
+      .then(_.trim),
+  })
+  .then((result) => {
+    var diff = jsdiff.diffTrimmedLines(result.actual, result.expected);
+    assert.equal(diff.length, 1);
+  });
+}
+
+getDirectories(__dirname)
+  .forEach(assertTransformation);
 
 // import {defineMessages} from "react-intl";
 //
