16fd7628c9ef6b67ab0a65ffca4c6b6521d598fc
Eric Baer <me@ericbaer.com>
2015-12-31 11:57:00 -0800

    Add linting + correct errors

--- src/index.js
+++ src/index.js
@@ -1,117 +1,116 @@
-"use strict";
-
 import crypto from "crypto";
 import path from "path";
 
-const FUNCTION_NAMES = ["defineMessages"]
+const FUNCTION_NAMES = ["defineMessages"];
 
-export default function({ types: t }) {
-  function referencesImport(path, mod, importedNames) {
-    if (!(path.isIdentifier() || path.isJSXIdentifier())) { return false; }
-    return importedNames.some((name) => path.referencesImport(mod, name));
-  }
+const i18nIdHashing = function ({ types: t }) {
+  const referencesImport = function referencesImport(pathNode, mod, importedNames) {
+    if (!(pathNode.isIdentifier() || pathNode.isJSXIdentifier())) { return false; }
+    return importedNames.some((name) => pathNode.referencesImport(mod, name));
+  };
 
   /**
    * @param  {Object}  opts - A Babel options object
    *
    * @return {Object}  Returns the module name to search for imports of. Defaults to `react-intl`
    */
-  function getModuleSourceName(opts) {
+  const getModuleSourceName = function getModuleSourceName(opts) {
     return opts.moduleSourceName || "react-intl";
-  }
+  };
 
   /**
    * @param  {Object}  opts - A Babel options object
    *
-   * @return {Object}  Returns the Identifier name to search for calls of. Defaults to `defaultMessages`
+   * @return {Object}  Returns the Identifier name to search for calls of. Defaults to
+   * `defaultMessages`
    */
-  function getMethodName(opts) {
+  const getMethodName = function getMethodName(opts) {
     return opts.methodName || "defaultMessages";
-  }
+  };
 
   /**
    * @param  {String}  hashKey - A string used to generate a SHA1 hash
    *
    * @return {Object}  A SHA1 hash of the hashKey
    */
-  function getHash(hashKey) {
+  const getHash = function getHash(hashKey) {
     return crypto
-      .createHash('sha1')
+      .createHash("sha1")
       .update(hashKey || "")
-      .digest('hex');
-  }
+      .digest("hex");
+  };
 
   /**
-   * @param  {ASTNode}  path - An AST node representing a POJO.
+   * @param  {ASTNode}  pathNode - An AST node representing a POJO.
    *
    * @return {Object}  A POJO representation of an AST node
    */
-  function generateObjectFromNode(path) {
-    return path.get('properties').map((prop) => [
-      prop.get('key').node.value,
-      prop.get('value').node.value,
+  const generateObjectFromNode = function generateObjectFromNode(pathNode) {
+    return pathNode.get("properties").map((prop) => [
+      prop.get("key").node.value,
+      prop.get("value").node.value
     ]).reduce((previousValue, property) => {
-      previousValue[property[0]] = property[1]
+      previousValue[property[0]] = property[1];
       return previousValue;
     }, {});
-  }
+  };
 
-  function processMessage(filename, messageObj) {
+  const processMessage = function processMessage(filename, messageObj) {
     if (!(messageObj[1] && messageObj[1].isObjectExpression())) {
       throw path.buildCodeFrameError(
-        `[babel-plugin-i18n-id-hashing] ${callee.node.name}() must be called with message ` +
+        `[babel-plugin-i18n-id-hashing] ${messageObj.node.name}() must be called with message ` +
         `descriptors defined as object expressions.`
       );
     }
 
     const objectIdProperty = messageObj[1]
-      .get('properties')
+      .get("properties")
       // Returns all an array for keyNode, ValueNode pairs
       .map((prop) => [
-        prop.get('key'),
-        prop.get('value'),
+        prop.get("key"),
+        prop.get("value")
       ])
       // Returns [[idKeyNode, idValueNode]]
       .filter((prop) => prop[0].node.value === "id")
       // Returns [idKeyNode, idValueNode]
       .pop()
       // Retuens idValueNode
-      [1];
+      [1]; //eslint-disable-line no-unexpected-multiline
 
     const objectProperties = generateObjectFromNode(messageObj[1]);
-    const generatedMessageId = `${getHash(filename)}.${objectProperties.id}`
+    const generatedMessageId = `${getHash(filename)}.${objectProperties.id}`;
 
     // Replace the Object's key with the generatedMessageId
     const objectKey = messageObj[0];
     objectKey.replaceWith(t.stringLiteral(generatedMessageId));
     // Replace the Object's `id` property with the generatedMessageId
     objectIdProperty.replaceWith(t.stringLiteral(generatedMessageId));
-  }
+  };
 
   return {
     visitor: {
       CallExpression(pathNode, state) {
         const moduleSourceName = getModuleSourceName(state.opts);
-        const callee = pathNode.get('callee');
+        const callee = pathNode.get("callee");
 
         // Return if the call expression is either
         //   - not found in a file that imports `react-intl`
         //   - is not a call to one of the FUNCTION_NAMES
-        if(referencesImport(callee, moduleSourceName, FUNCTION_NAMES) === false) { return; }
+        if (referencesImport(callee, moduleSourceName, FUNCTION_NAMES) === false) { return; }
 
         // FUNCTION_NAMES functions are of the form function(Object messages)
         // https://github.com/yahoo/react-intl/blob/2fdf9e7e695fa04673573d72ab6265f0eef3f98e/src/react-intl.js#L25-L29
-        let messagesObj = pathNode.get('arguments')[0];
+        const messagesObj = pathNode.get("arguments")[0];
 
         // Use a relative path to ensure hash key is the same on any system
         const filePath = path.relative(__dirname, state.file.opts.filename);
 
         // Process each message
         messagesObj
-          .get('properties')
+          .get("properties")
           .map((prop) => [
-            prop.get('key'),
-            prop.get('value'),
+            prop.get("key"),
+            prop.get("value")
           ])
           .forEach(processMessage.bind(null, filePath));
       },
@@ -123,17 +122,21 @@ export default function({ types: t }) {
         // Use a relative path to ensure hash key is the same on any system
         const filePath = path.relative(__dirname, state.file.opts.filename);
 
-        let accessor = pathNode.get("property");
+        const accessor = pathNode.get("property");
 
         if (accessor.type === "StringLiteral") {
-          accessor.replaceWith(t.stringLiteral(`${getHash(filePath)}.${accessor.node.value}`))
+          accessor.replaceWith(t.stringLiteral(`${getHash(filePath)}.${accessor.node.value}`));
         } else {
           // Convert xMemberExpression.identifier -> xMemberExpression[identifier]
-          pathNode.node.computed = true
+          pathNode.node.computed = true;
           // Add the hash to the result of any Identifier or Expression
-          accessor.replaceWith(t.binaryExpression("+", t.stringLiteral(getHash(filePath)), accessor.node))
+          accessor.replaceWith(
+            t.binaryExpression("+", t.stringLiteral(getHash(filePath)), accessor.node)
+          );
         }
       }
     }
   };
 };
+
+export default i18nIdHashing;
