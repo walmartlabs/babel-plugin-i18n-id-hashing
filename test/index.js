29e7e3fea78f16591519c963dda9c7aed4304b9b
Eric Baer <me@ericbaer.com>
2016-01-05 15:38:22 -0800

    Remove chai

--- test/index.js
+++ test/index.js
@@ -1,8 +1,5 @@
 var _ = require("lodash");
-var chai = require("chai");
 var assertTransform = require("./assert-transform.js");
-
-var expect = chai.expect;
 
 var babelOptions = {
   "presets": [ "es2015" ],
