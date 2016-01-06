5e91cebeb8ffea016c4b1482d56723a9ed6e453e
Eric Baer <me@ericbaer.com>
2016-01-05 17:34:25 -0800

    Extract testing tool into new library

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 var _ = require("lodash");
+var assertTransform = require("assert-transform");
 var babel = require("babel-core");
-
-var assertTransform = require("./assert-transform.js");
+var path = require("path");
 
 var BABEL_OPTIONS = {
   "presets": [ "es2015" ],
@@ -18,24 +18,36 @@ describe("babel-plugin-i18n-id-hashing", function() {
   })
 
   it("should change the key and the id property in defineMessages", function () {
-    return assertTransform("./define-messages/actual.js", "./define-messages/expected.js", BABEL_OPTIONS);
+    return assertTransform(
+      path.join(__dirname, "./define-messages/actual.js"),
+      path.join(__dirname, "./define-messages/expected.js"), BABEL_OPTIONS);
   });
   it("should transform dot property access to specified method", function () {
-    return assertTransform("./dot-property-accessor/actual.js", "./dot-property-accessor/expected.js", BABEL_OPTIONS);
+    return assertTransform(
+      path.join(__dirname, "./dot-property-accessor/actual.js"),
+      path.join(__dirname,"./dot-property-accessor/expected.js"), BABEL_OPTIONS);
   });
   it("should transform inline expression accessors", function () {
-    return assertTransform("./expression-accessor/actual.js", "./expression-accessor/expected.js", BABEL_OPTIONS);
+    return assertTransform(
+      path.join(__dirname, "./expression-accessor/actual.js"),
+      path.join(__dirname,"./expression-accessor/expected.js"), BABEL_OPTIONS);
   });
   it("should transform a jsx file", function () {
     var jsxBabelOpts = _.clone(BABEL_OPTIONS);
     jsxBabelOpts.presets.push("react");
-    return assertTransform("./jsx-define-messages/actual.jsx", "./jsx-define-messages/expected.js", jsxBabelOpts);
+    return assertTransform(
+      path.join(__dirname, "./jsx-define-messages/actual.jsx"),
+      path.join(__dirname,"./jsx-define-messages/expected.js"), jsxBabelOpts);
   });
   it("should transform string literal accessors", function () {
-    return assertTransform("./string-literal-accessor/actual.js", "./string-literal-accessor/expected.js", BABEL_OPTIONS);
+    return assertTransform(
+      path.join(__dirname, "./string-literal-accessor/actual.js"),
+      path.join(__dirname,"./string-literal-accessor/expected.js"), BABEL_OPTIONS);
   });
   it("should transform variable accessors", function () {
-    return assertTransform("./variable-accessor/actual.js", "./variable-accessor/expected.js", BABEL_OPTIONS);
+    return assertTransform(
+      path.join(__dirname, "./variable-accessor/actual.js"),
+      path.join(__dirname,"./variable-accessor/expected.js"), BABEL_OPTIONS);
   });
   describe("options", function () {
     // -    ["../lib/index.js", {
